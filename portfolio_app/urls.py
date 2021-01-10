b24738176addfc00073e274c5eff3b854687b934
Carlos Salgado <socd06@gmail.com>
2021-01-10 17:42:33 -0500

    remove api import causing error from urls

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from rest_framework import routers
 #from .api import ProjectInfoViewSet
+# CAUSING ERROR
 
 urlpatterns = [
     path('',views.index,name='index'),
